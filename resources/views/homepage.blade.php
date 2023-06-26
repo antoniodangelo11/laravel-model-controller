636a6ef21066d920b83dcaac71292484ba751b34
antoninodangelo11 <reapercsp@gmail.com>
2023-06-26 18:38:56 +0200

    add bonus

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.base')
 
 @section('contents')
-    <div class="container">
-        <div class="row row-cols-4">
+<section class="bg-body-secondary" style="height: 100vh">
+    <div class="container" style="height: 100vh">
+        <div class="row row-cols-5">
             @foreach ($movies as $movie)
                 <div class="col py-4">
                     @include('partials.card')
@@ -10,4 +11,5 @@
             @endforeach
         </div>
     </div>
+</section>
 @endsection
--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,9 +1,14 @@
-<div class="card h-100" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie->original_title }}</h6>
-        <p class="card-text">Nazionalità: {{ $movie->nationality}}</p>
-        <p class="card-text">Data: {{ $movie->date}}</p>
-        <p class="card-text">Voto: {{ $movie->vote}}</p>
+<div class="myCard">
+    <div class="innerCard">
+        <div class="frontSide">
+            <img class="h-100 rounded w-100" src="{{ Vite::asset('resources/img/' . $movie->img) }}" alt="">
+        </div>
+        <div class="backSide rounded">
+            <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie->original_title }}</h6>
+            <p class="card-text">Nazionalità: {{ $movie->nationality}}</p>
+            <p class="card-text">Data: {{ $movie->date}}</p>
+            <p class="card-text">Voto: {{ $movie->vote}}</p>
+        </div>
     </div>
 </div>
